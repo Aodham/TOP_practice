7fa5a214250b335da08d77dad662839c3e856e0e
Aodham <aodham@protonmail.com>
2020-01-05 22:09:31 +0100

    Adds fundamentals_3 solved excercises

---
+++ web_development_101/javascript/fundamentals_3/scripts.js
@@ -0,0 +1,15 @@
+function add7 (number){
+    return number + 7;
+};
+
+function multiply (n1, n2) {
+    return n1 * n2;
+};
+
+function capitalize (text) {
+    return text.charAt(0).toUpperCase() + text.slice(1);
+}
+
+function lastletter (text) {
+    return text.slice(-1);
+}
